a63799b1edb9c4e8b7e711c40ddc4dadccc0c439
Chisom Ogbonna <mavreon@gmail.com>
2022-07-06 17:03:18 +0100

    Events and working with states(React Hooks)

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,36 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
+
+
 const ExpenseItem= (props) => {
+    //React hooks must only be called directly inside react component functions...
+    /*
+    UseState(parameter) returns an array of two elements where the first 
+    element is the current state value and the second 
+    element is a function that is to be called on changing the 
+    value assigned to the first element.
+    */
+    const [title, setTitle] = useState(props.title);
+    console.log('New State:' + title);
+
+    const clickHandler = ()=>{
+        //Won't be executed immediately until this component function is re-executed...
+        setTitle('Updated!');
+        //console.log(title);
+    };
+
     return(
         <Card className='expense-item'>
            <ExpenseDate date={props.date}></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            {/* Just point to the name of the function not state it directly */}
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
