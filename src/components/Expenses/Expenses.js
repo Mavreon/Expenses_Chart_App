0be5977e1a2ff60b5c82c4e8183cfb8bb5114c4b
Chisom Ogbonna <mavreon@gmail.com>
2022-07-11 06:47:40 +0100

    Finished Updates

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import "./Expenses.css"
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "../Chart/ExpensesChart";
 
 const Expenses = (props) => {
 
@@ -19,6 +20,7 @@ const Expenses = (props) => {
     return(
         <Card className="expenses">
             <ExpensesFilter onFilterChange ={filterChangeHandler} value={filteredYear}></ExpensesFilter>
+            <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
             <ExpensesList filteredExpenses = {filteredExpenses}></ExpensesList>
         </Card>
     );
